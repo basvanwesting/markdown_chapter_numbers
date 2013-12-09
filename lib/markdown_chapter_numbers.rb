9a12764b243f47833bbf2af730ac3a4265e60beb
basvanwesting <basvanwesting@gmail.com>
2013-12-09 17:39:36 +0100

    add bin/update_markdown_chapter_numbers

--- lib/markdown_chapter_numbers.rb
+++ lib/markdown_chapter_numbers.rb
@@ -1,9 +1,9 @@
-require "markdown_chapter_numbers/version"
-require "markdown_chapter_numbers/update"
+require_relative "markdown_chapter_numbers/version"
+require_relative "markdown_chapter_numbers/update"
 
 module MarkdownChapterNumbers
 
-  def update(input_filename, output_filename)
+  def self.update(input_filename, output_filename)
     File.open(output_filename,'w') do |output|
       File.open(input_filename,'r') do |input|
         Update.new.call(input,output)
